873ba36a6a1ae8b91ec4760272280ae242728dd6
Alex Salom <alex.salom@gmail.com>
2015-05-19 12:43:19 +0200

    refactor to use OCMock

--- MyBrowseOverflowTests/Logic/QuestionCreationTests.m
+++ MyBrowseOverflowTests/Logic/QuestionCreationTests.m
@@ -9,35 +9,31 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import "StackOverflowManager.h"
+#import "StackOverflowManagerDelegate.h"
 #import "StackOverflowCommunicator.h"
 #import "MockStackOverflowManagerDelegate.h"
 #import "MockStackOverflowCommunicator.h"
 #import "Topic.h"
 #import <OCMock/OCMock.h>
 
-@interface QuestionCreationTests : XCTestCase
+@interface QuestionCreationTests : XCTestCase <StackOverflowManagerDelegate>
 
 @end
 
 @implementation QuestionCreationTests {
     StackOverflowManager *_manager;
-    MockStackOverflowManagerDelegate *_delegate;
     NSError *_underlyingError;
 }
 
 - (void)setUp {
     [super setUp];
     _manager = [[StackOverflowManager alloc] init];
-    _delegate = [[MockStackOverflowManagerDelegate alloc] init];
-    _manager.delegate = _delegate;
-    _underlyingError = [NSError errorWithDomain:@"Test domain" code:0 userInfo:nil];
-    
+    _manager.delegate = self;
 }
 
 - (void)tearDown {
     _manager = nil;
     _manager.delegate = nil;
-    _delegate = nil;
     _underlyingError = nil;
     [super tearDown];
 }
@@ -55,38 +51,44 @@
     XCTAssertNoThrow(_manager.delegate = nil, @"Object confirming to the delegate protocol should be used");
 }
 
-#pragma mark - Custom Mocks
-- (void)testAskingForQuestionsMeansRequestingData {
-    MockStackOverflowCommunicator *communicator = [[MockStackOverflowCommunicator alloc] init];
-    _manager.communicator = communicator;
-    Topic *topic = [[Topic alloc] initWithName:@"iPhone" tag:@"iphone"];
-    [_manager fetchQuestionsOnTopic:topic];
-    XCTAssertTrue(communicator.wasAskedToFetchQuestions, @"The communicator should need to fetch data");
+- (void)testAskingForQuestionsMeansRequestingData_OCMock {
+    // when
+    id communicatorMock = OCMClassMock([StackOverflowCommunicator class]);
+    _manager.communicator = communicatorMock;
+    OCMExpect([communicatorMock searchForQuestionsWithTag:nil]);
+    
+    // given
+    [_manager fetchQuestionsOnTopic:nil];
+    
+    // then
+    OCMExpect(communicatorMock);
 }
 
 - (void)testErrorReturnedToDelegateIsNotErrorNotifiedByCommunicator {
-    [_manager searchingForQuestionsFailedWithError:_underlyingError];
-    XCTAssertFalse(_underlyingError == [_delegate fetchError], @"Error should be at the correct level of abstraction");
+    // when
+    NSError *error = [NSError new];
+    
+    // given
+    [_manager searchingForQuestionsFailedWithError:error];
+    
+    //then
+    XCTAssertFalse(_underlyingError == error, @"Error should be at the correct level of abstraction");
 }
 
 - (void)testErrorReturnedToDelegateDocumentsUnderlyingError {
-    [_manager searchingForQuestionsFailedWithError:_underlyingError];
-    XCTAssertEqualObjects([[[_delegate fetchError] userInfo] objectForKey:NSUnderlyingErrorKey], _underlyingError, @"The underlying error should be available to client code");
-}
-
-#pragma mark - OCMock
-- (void)testAskingForQuestionsMeansRequestingData_OCMock {
     // when
-    Topic *topic = [[Topic alloc] initWithName:@"iPhone" tag:@"iphone"];
-    id communicatorMock = OCMClassMock([StackOverflowCommunicator class]);
-    _manager.communicator = communicatorMock;
-    [[communicatorMock expect] searchForQuestionsWithTag:topic.tag];
+    NSError *error = [NSError errorWithDomain:@"Test domain" code:0 userInfo:nil];
     
-    // given
-    [_manager fetchQuestionsOnTopic:topic];
+    //given
+    [_manager searchingForQuestionsFailedWithError:error];
     
     // then
-    [communicatorMock verify];
+    XCTAssertEqualObjects([[_underlyingError userInfo] objectForKey:NSUnderlyingErrorKey], error, @"The underlying error should be available to client code");
+}
+
+#pragma mark -- StackOverflowManagerDelegate
+- (void)fetchingQuestionsFailedWithError:(NSError *)error {
+    _underlyingError = error;
 }
 
 @end
